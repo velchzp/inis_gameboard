2fd5a9c2bf14a84eb31589e7a5ac67dc4ac7c9de
Velerii Havryliuk <velch275@gmail.com>
2023-11-19 22:01:52 +0300

    U

--- src/App.tsx
+++ src/App.tsx
@@ -23,8 +23,7 @@ function App() {
     socket.emit("my-deck-update");
     socket.emit("map-update");
     socket.emit("allPlayers-info");
-    socket.emit("game-update");
-    socket.emit("dealCards-update");
+
     // dispatch(fetchLobbyInfo());
     // dispatch(fetchHexagons());
     // dispatch(fetchSidebarInfo());
