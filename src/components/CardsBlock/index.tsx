97551d20597a048aea9417cc926b9427df00f12a
Velerii Havryliuk <velch275@gmail.com>
2023-11-19 20:00:36 +0300

    Right_test2

--- src/components/CardsBlock/index.tsx
+++ src/components/CardsBlock/index.tsx
@@ -35,14 +35,8 @@ export const CardsBlock = () => {
     socket.emit("player-card-info", {
       cardId: cardID,
     });
+    dispatch(fetchCardInfo());
   };
-
-  useEffect(() => {
-    socket.emit("game-update");
-    socket.on("game-update", (data) => {
-      console.log(data);
-    });
-  }, []);
 
   useEffect(() => {
     socket.on("my-deck-update", (deckinfo: IMyDeckUiInfo) => {
